e6a453b2229762cd14d15a3933b6f5349244e969
Dmitri Lapin <dmitri.lapin@gmail.com>
2018-05-21 08:51:18 +0300

    Added license to setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-"""nvidia-power-manager package setup script"""
+# -*- coding: utf-8 -*-
+
+# This file is part of NVIDIA Power Manager - indicator applet for NVIDIA
+# laptops.
+# Copyright (C) 2018 Dmitri Lapin
+#
+# This work is based on the NVIDIA Power Indicator applet by
+# André Brait Carneiro Fabotti
+#
+# NVIDIA Power Manager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# NVIDIA Power Manager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NVIDIA Power Manager. If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup, find_packages
 
